9c9569c767b04b7ba739c3dc45cf4b20063c7206
Gustavo <gu.paulino2007@gmail.com>
2025-08-04 21:40:00 -0300

    Ai calica o negocio funcionou

--- CadastroUsuário.java
+++ CadastroUsuário.java
@@ -12,29 +12,48 @@ import java.awt.event.ActionListener;
 public class CadastroUsuário {
     public static void main(String[] args) {
         JFrame janelaP = new JFrame("Cadastro de Usuário");
-        janelaP.setSize(700,600);
+        janelaP.setSize(400,300);
         janelaP.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janelaP.setLocationRelativeTo(null);
-        janelaP.setLayout(new GridLayout(2,4,5,5));
+        janelaP.setLayout(new GridLayout(4,2,10,10));
     
         JLabel Nome= new JLabel("Nome");
+        Nome.setHorizontalAlignment(JLabel.CENTER);
         JLabel Email= new JLabel("Email");
+        Email.setHorizontalAlignment(JLabel.CENTER);
         JLabel Idade= new JLabel("Idade");
+        Idade.setHorizontalAlignment(JLabel.CENTER);
 
-        JTextField areaNome = new JTextField(20);
-        JTextField areaEmail = new JTextField(20);
-        JTextField areaIdade = new JTextField(20);
+        JTextField areaNome = new JTextField(8);
+        JTextField areaEmail = new JTextField(8);
+        JTextField areaIdade = new JTextField(8);
 
         JButton Cadastrar = new JButton("Cadastrar");
 
         Cadastrar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
-               JOptionPane.showMessageDialog(janelaP, 
-                   "Usuário cadastrado com sucesso!\n" +
-                   "Nome: " + areaNome.getText() + "\n" +
-                   "Email: " + areaEmail.getText() + "\n" +
-                   "Idade: " + areaIdade.getText());
+                if (areaNome.getText().isEmpty() ||
+                        areaEmail.getText().isEmpty() ||
+                        areaIdade.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(janelaP,"Um ou mais campos estão vazios!!", "Cadastro Incompleto.", JOptionPane.ERROR_MESSAGE );
+                }else {
+                    JOptionPane.showMessageDialog(janelaP,
+                            "Usuário cadastrado com sucesso!\n" +
+                                    "Nome: " + areaNome.getText() + "\n" +
+                                    "Email: " + areaEmail.getText() + "\n" +
+                                    "Idade: " + areaIdade.getText());//FUNCIONOUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU SOU UM GENIO
+                }
         }
     });
+        janelaP.add(Nome);
+        janelaP.add(areaNome);
+        janelaP.add(Email);
+        janelaP.add(areaEmail);
+        janelaP.add(Idade);
+        janelaP.add(areaIdade);
+        janelaP.add(new JLabel(""));
+        janelaP.add(Cadastrar);
+        janelaP.setVisible(true);
+
 }
 }
